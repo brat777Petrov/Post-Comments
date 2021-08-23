f6ba416a058f07f81ecae6423fed72a5dccf841e
Sasha Petrov <brat77@tut.by>
2021-08-23 18:23:37 +0300

    refactor: clear code

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function httpGetRequest(url, params, callback) {
-  /**
-   * аргументы:
-   * url - адрес сервера
-   * params - объект, в котором хранятся параметры для запроса
-   * этот объект должен быть преобразован в строку и добавлен к url после символа '?'
-   * callback - ф-ция которая будет вызвана после того как запрос выполнен и данные удачно распарсились
-   * callback должен вызываться так:
-   * если случилась какая-то ошибка то callback(error), если ошибки не было то callback(null, data);
-   * использовать XmlHttpRequest 
-   * https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest
-   *  
-   */
+
+  const url = 'https://jsonplaceholder.typicode.com';
+  const main = document.querySelector('main');
+  let flagRemovePanel = false;
+    
+  httpGetRequest(url,'/posts', start);
+  
+  function httpGetRequest(url, params, callback) {
     const fullUrl = url + params;
     let x = new XMLHttpRequest();
     x.open("GET", fullUrl);
@@ -23,12 +18,7 @@ function httpGetRequest(url, params, callback) {
       callback(error);
     };                  
   }
-  const url = 'https://jsonplaceholder.typicode.com';
-  httpGetRequest(url,'/posts', start);
-  
-  const main = document.querySelector('main');
-  flagRemovePanel = false;
-  
+
   function start(z,response) {
     const data = JSON.parse(response);
     shortPosts(100, data);
@@ -70,13 +60,11 @@ function httpGetRequest(url, params, callback) {
 
   function openPost (data) {
     const postId = event.target.classList[1];
-    console.log(event.target);
     if (postId) {
       const checkPanel = document.getElementById('panelComments');
       if (checkPanel) {
         clearComments();
       }
-     
         addCommentBtn(postId);
         const onePost = document.createElement('div');
         main.append(onePost);
@@ -116,12 +104,7 @@ function httpGetRequest(url, params, callback) {
       panelComments.append(comment);
       comment.className = ('comment');
       comment.innerHTML = (arrCommit[i].body);
-      // console.log(postId,comments[postId]);
     }
-     
-
-
-
   }
 
   function clearComments() {
